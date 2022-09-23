51a54ddb5fad39057dbb9ab449d90d55aa0ef184
kharisma2121 <kharismaistiqomah20@gmail.com>
2022-09-23 15:19:02 +0700

    terserah

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -3,6 +3,6 @@
       public function index()
        {
             echo "<h1>Perkenalkan</h1>"; 
-            echo"Nama saya kharismaaaaaaa";
+            echo"Nama saya kharismmmmmmmmmm";
        }
     }
