23fabf703faefbdf3e46829abf289972e1078c83
kharisma2121 <kharismaistiqomah20@gmail.com>
2022-10-07 14:28:39 +0700

    rev membuat controller dan view

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <meta charset=”utf-8”> 
+    <title>Web Prog II | Merancang Template sederhana dengan 
+codeigniter</title> 
+    <link rel=”stylesheet” type=”text/css” href=”<?php echo 
+base_url() ?>assets/css/stylebuku.css”> 
+</head> 
+<body> 
+    <div id=”wrapper”> 
+        <header> 
+            <hgroup> 
+                <h1>RentalBuku.net</h1> 
+                <h3>Membuat Template Sederhana dengan 
+CodeIgniter</h3> 
+            </hgroup> 
+            <nav> 
+                <ul>   
+                    <li><a href=”<?php echo 
+base_url().’index.php/web’ ?>”>Home</a></li> 
+                    <li><a href=”<?php echo 
+base_url().’index.php/web/about’ ?>”>About</a></li> 
+                </ul> 
+            </nav> 
+            <div class=”clear”></div> 
+        </header>
 <section> 
             <h1><?php echo $judul ?></h1> 
             <p align=’justify’>Pada pengertian codeigniter di atas 
@@ -47,3 +74,10 @@ menangani bagian controller dan modelnya. Jadi pembagian tugas pun
 menjadi mudah dan pengembangan aplikasi dapat di lakukan dengan 
 cepat dan terstruktur.</p> 
         </section>
+        
+        <footer> 
+            <a href=”http://www.RentalBuku.com”>RentalBuku</a> 
+        </footer> 
+    </div> 
+</body> 
+</html>
