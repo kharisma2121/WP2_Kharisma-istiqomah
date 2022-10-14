e2e6a4780a1dc54689eec784003574691c7629af
kharisma2121 <kharismaistiqomah20@gmail.com>
2022-10-14 15:38:02 +0700

    membuat validasi

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -6,8 +6,7 @@
 
 <body> 
     <center> 
-        <form action="<?= base_url('matakuliah/cetak');  ?>" 
-method="post"> 
+        <form action="<?= base_url('matakuliah/cetak'); ?>"method="post">
             <table> 
                 <tr> 
                     <th colspan="3"> 
@@ -16,42 +15,44 @@ method="post">
                 </tr>
                 <tr> 
                     <td colspan="3"> 
-                        <hr> 
+                        <hr>
                     </td> 
                 </tr> 
                 <tr> 
                     <th>Kode MTK</th> 
                     <th>:</th> 
                     <td> 
-                        <input type="text" name="kode" id="kode"> 
+                        <input type="text" name="kode" id="kode">
+                        <?= form_error('kode','<small class ="text-danger pl-3">','<small>'); ?>
                     </td> 
                 </tr> 
                 <tr> 
                     <th>Nama MTK</th> 
                     <td>:</td> 
                     <td> 
-                        <input type="text" name="nama" id="nama"> 
+                        <input type="text" name="nama" id="nama">
+                        <?= form_error('nama','<small class ="text-danger pl-3">','<small>'); ?>
                     </td> 
                 </tr> 
                 <tr> 
                     <th>SKS</th> 
                     <td>:</td> 
                     <td> 
-                        <select name="sks" id="sks"> 
-                            <option value="">Pilih SKS</option> 
-                            <option value="2">2</option> 
-                            <option value="3">3</option> 
-                            <option value="4">4</option> 
-                        </select> 
+                        <select name="sks" id="sks">
+                            <option value="">Pilih SKS</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
                     </td> 
                 </tr> 
                 <tr> 
                     <td colspan="3" align="center"> 
-                        <input type="submit" value="Submit"> 
+                        <input type="submit" value="submit">
                     </td> 
                 </tr> 
-            </table> 
-        </form> 
+            </table>
+        </form>     
     </center> 
 </body>
 
